45a5520a7c3f45802767440f767032b503de361e
WanderingZ <40839581+WanderingZ@users.noreply.github.com>
2022-06-23 19:45:04 -0400

    commit ep 4 (to finish later)

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -5,11 +5,15 @@ public class Block extends Rectangle {
     int dx = 3;
     int dy = -3;
     boolean destroyed = false;
-    Block(int a, int c, int w, int h, String s) {
+    Rectangle left, right;
+    boolean powerup = false;
+    Block(int a, int b, int w, int h, String s) {
         x = a;
-        y = c;
+        y = b;
         width = w;
         height = h;
+        left = new Rectangle(a-1, b, 1, h);
+        right = new Rectangle(a+w+1, b, 1, h);
         pic = Toolkit.getDefaultToolkit().getImage(s);
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,9 @@ import javax.swing.*;
 import static javax.swing.JFrame.*;
 
 public class Main {
+    static JFrame frame = new JFrame("Block Breaker");
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Block Breaker");
+
 
         Logger.log("hello, world!");
 
@@ -13,9 +14,9 @@ public class Main {
         frame.getContentPane().add(panel);
 
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        JOptionPane jOptionPane = new JOptionPane("");
         frame.setVisible(true);
-
+        frame.setLocationRelativeTo(null);
         frame.setSize(500, 600);
         frame.setResizable(false);
     }
